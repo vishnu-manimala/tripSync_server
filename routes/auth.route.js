dddcc7dceebf7256e470430b0c441a99904feaba
vishnuManimala <vishnumanimala8133@gmail.com>
2023-11-08 07:18:57 +0530

    added admin side

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -9,4 +9,7 @@ auth_route.post('/password_login',authController.passwordLogin);
 auth_route.post('/send_otp',authController.sendOtp);
 auth_route.post('/verify_otp',authController.authOtp);
 auth_route.post('/reset_password',authController.resetPassword);
+
+
+
 module.exports = auth_route;
